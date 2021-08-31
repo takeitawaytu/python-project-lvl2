9d7381e11817369a74d262c30ab8a30b01731789
takeitawaytu <takeitawaytu@gmail.com>
2021-08-31 18:26:27 +0300

    fix plain render

--- gendiff/renders/plain.py
+++ gendiff/renders/plain.py
@@ -1,26 +1,35 @@
 from gendiff import engine
+from gendiff.renders.json_render \
+    import get_lower, get_value
+
+
+def make_converted(node):
+    if isinstance(node, dict):
+        return '[complex value]'
+    elif isinstance(node, str):
+        if node in ('null', 'true', 'false'):
+            return '{}'.format(node)
+        return '\'{}\''.format(node)
+    return get_lower(node)
 
 
 def render(node, level=''):
     result = []
     for key, val in sorted(node.items()):
-        value = val.get(engine.VALUE)
-        status = val.get(engine.STATUS)
+        status, value = get_value(val)
+        converted = make_converted(value)
         if status == engine.ADDED:
-            if isinstance(value, dict):
-                value = engine.COMPLEX_VALUE
-            result.append('Property \'{}{}\' was added with value \'{}\''
-                          .format(level, key, value))
+            result.append('Property \'{}{}\' was added with value: {}'
+                          .format(level, key, converted))
         elif status == engine.REMOVED:
             result.append('Property \'{}{}\' was removed'
                           .format(level, key))
         elif status == engine.CHANGED:
-            result.append('Property \'{}{}\' was changed. From \'{}\' to \'{}\''
+            result.append('Property \'{}{}\' was updated. From {} to {}'
                           .format(level, key,
-                                  val[engine.OLD_VALUE],
-                                  val[engine.NEW_VALUE]))
+                                  make_converted(val[engine.OLD_VALUE]),
+                                  make_converted(val[engine.NEW_VALUE])
+                                  ))
         elif status == engine.COMPLEX_VALUE:
-            level += key + '.'
-            result.append(render(value, level))
-            level = ''
+            result.append(render(value, level + '{}.'.format(key)))
     return '\n'.join(result)
