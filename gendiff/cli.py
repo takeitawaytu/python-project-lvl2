ea5079bb81c7f217e642e4bfb9b657f435ce9ecb
takeitawaytu <takeitawaytu@gmail.com>
2021-09-01 11:15:07 +0300

    fix cli

---
+++ gendiff/cli.py
@@ -0,0 +1,17 @@
+import argparse
+from gendiff.renders import FORMAT
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='Generate differences')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format',
+                        action='store',
+                        dest='format',
+                        metavar='FORMAT',
+                        default='stylish',
+                        help='set format of output: "stylish", "plain"',
+                        choices=['plain', 'json', 'stylish'],
+                        type=str)
+    return parser.parse_args()
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
-from gendiff.app import generate_diff
-from gendiff.renders import FORMAT
-import argparse
+from gendiff import generate_diff
+from gendiff.cli import get_parser
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate differences')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format',
-                        action='store',
-                        dest='format',
-                        metavar='FORMAT',
-                        default='stylish',
-                        help='set format of output: "stylish", "plain"',
-                        type=FORMAT.get)
-    args = parser.parse_args()
+    args = get_parser()
     result = generate_diff(args.first_file, args.second_file, args.format)
     print(result)
 
